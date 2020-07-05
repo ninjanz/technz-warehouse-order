aef2c938f738695a5d8201ab27c626884c25c2f2
ninjanz <abc@test.com>
2020-07-05 13:25:12 +0800

    update

--- telegram_funcs.js
+++ telegram_funcs.js
@@ -1,6 +1,6 @@
-import * as TelegramBot from 'node-telegram-bot-api';
+import * as pkg from 'node-telegram-bot-api';
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new pkg.TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
 
 async function sendDoc(doc) {
     bot.sendDocument("-400162180", doc)
