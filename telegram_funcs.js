589c662fcacc46b81a91b9b27e3eec16c92b59d1
ninjanz <abc@test.com>
2020-07-05 13:09:16 +0800

    updated to es6

--- telegram_funcs.js
+++ telegram_funcs.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api')
+import { TelegramBot } from 'node-telegram-bot-api';
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
 
 async function sendDoc(doc) {
