649ec9fd786adf549dcb53ddbe09e0c5d66390ce
ninjanz <abc@test.com>
2020-07-05 21:59:05 +0800

    update

--- app.mjs
+++ app.mjs
@@ -31,11 +31,11 @@ app.post('/create-invoice', function (req, res) {
 })
 
 app.get('/send-doc', (req, res) => {
-    //updateToken()
-    //.then(qbo.findInvoices())
+    updateToken()
+    .then(qbo.findInvoices())
     //.then((invObj) => qbo.getInvoicePdf(invObj.QueryResponse.Invoice[0].Id))
-    qbo.findInvoices().then(doc => { console.log(doc) })
-    //.then((doc) => sendDoc.sendDoc(doc))
+    //qbo.findInvoices().then(doc => { console.log(doc) })
+    .then((doc) => sendDoc.sendDoc(doc))
     //.catch((err) => console.log(err))
 })
 
