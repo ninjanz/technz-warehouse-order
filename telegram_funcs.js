2beb5e67423095cbbb50ea22c2f1e1b086d1a4e7
ninjanz <abc@test.com>
2020-07-06 17:19:14 +0800

    udpate

--- telegram_funcs.js
+++ telegram_funcs.js
@@ -2,11 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
 async function sendDoc(doc) {
-    opts = {
-        "fileOptions": {
-            "contentType": "application/pdf"
-        }
-    }
+    opts = { "contentType": "application/pdf" }
+    
     await bot.sendMessage("-400162180", "doc get")
     await bot.sendDocument("-400162180", doc, {}, opts)
     console.log("doc sent!")
