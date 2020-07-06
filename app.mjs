129dc9e13defa8517442ccad53df24caba492e25
ninjanz <abc@test.com>
2020-07-06 09:15:42 +0800

    update

--- app.mjs
+++ app.mjs
@@ -36,9 +36,9 @@ app.get('/send-doc', async (req, res) => {
     let invoices = await qbo.findInvoices()
     //.then((invObj) => qbo.getInvoicePdf(invObj.QueryResponse.Invoice[0].Id))
     console.log(invoices)
-    let doc = await qbo.getInvoicePdf({"Id": invoices.QueryResponse.Invoice[0].Id})
-    console.log(doc) 
-    tel.sendDoc(doc)
+    //let doc = 
+    //console.log(doc) 
+    tel.sendDoc(qbo.getInvoicePdf({"Id": invoices.QueryResponse.Invoice[0].Id}))
     //.
   } catch(err)  { console.log(err) }
 })
