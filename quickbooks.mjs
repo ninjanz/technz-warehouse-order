238020378c9742bd334735511307ffb69ab74ad0
ninjanz <abc@test.com>
2020-08-09 10:48:45 +0800

    update

--- quickbooks.mjs
+++ quickbooks.mjs
@@ -199,8 +199,9 @@ async function _createOrderPdf(_accepted, _rejected) {
 
     const doc = printer.createPdfKitDocument(docDefinition)
     const doc64 = new OutputDocument(doc)
+    doc_buffer = await doc64.getBuffer()
 
-    return doc64.getBuffer()
+    return doc_buffer
 }
 
 async function createTable(someArray, tableHeader) {
