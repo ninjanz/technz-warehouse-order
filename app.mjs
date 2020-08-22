b70bc69462e67262b55c9b619efa6ba8eda3ca85
ninjanz <nzzainal1713@gmail.com>
2020-08-22 15:57:43 +0800

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 
 //import { qbo, createInvoice, updateToken } from "./quickbooks.mjs";
 import { invoiceQ } from "./queue.mjs";
-import { _findLastInv as findInv } from ".quickbooks.mjs";
+import { _findLastInv as findInv, updateToken } from ".quickbooks.mjs";
 
 // setup express with body-parser
 const app = express()
@@ -71,6 +71,7 @@ app.get('/job/:id', async (req, res) => {
 });
 
 app.get('/newInvNum', async (req, res) => {
+  await updateToken()
   let inv = await findInv()
 
   console.log(inv)
